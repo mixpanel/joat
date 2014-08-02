9abbb47e612c6268d43fc011a9c4deae80314e2f
R. Kevin Nelson <kevin@rkn.la>
2014-08-01 21:30:05 -0400

    Add params tests, stabilize success case
    The JWT library (or possibly python) doesn't guarantee dict key/value
    ordering, so encoding the token isn't guaranteed to result in the same
    thing every time.

--- tests/test_joat.py
+++ tests/test_joat.py
@@ -44,15 +44,8 @@ class TestJOAT(unittest.TestCase):
         'name': 'My OAuth2 Provider'
       },
       'user_id': '12345',
-      'scope': ['email', 'profile']
+      'authorized_scope': ['email', 'profile']
     }
-
-
-  def test_generate_without_salt(self):
-    joat = JOAT('My OAuth2 Provider', client_id='abc123DEF')
-    with self.assertRaises(NotImplementedError):
-      token = joat.issue_token(user_id='12345', scope=['email', 'profile'])
-
 
   def test_generate_token(self):
     joat = JOAT('My OAuth2 Provider', client_id='abc123DEF')
@@ -63,5 +56,37 @@ class TestJOAT(unittest.TestCase):
         issued_at=self.test_iat_datetime,
         lifetime=self.test_exp_timedelta)
 
-    self.assertEqual(token, self.jwt_token)
+    try:
+      decoded = jwt.decode(token, self.generate_salt(None))
+    except:
+      self.fail("Token was not decodable after generation")
 
+    reference = jwt.decode(self.jwt_token, self.generate_salt(None))
+
+    # this borders on tautological, but the token should be a JWT that's decodable, with the same claims as the test data
+    self.assertDictEqual(decoded, reference)
+
+  def test_generate_without_salt(self):
+    joat = JOAT('My OAuth2 Provider', client_id='abc123DEF')
+    with self.assertRaises(NotImplementedError):
+      token = joat.issue_token(user_id='12345', scope=['email', 'profile'])
+
+  def test_generate_token_missing_params(self):
+    joat = JOAT('My OAuth2 Provider')
+    joat.salt_generator = self.generate_salt
+
+    token = joat.issue_token()
+    self.assertIsNone(token)
+
+    joat.client_id="abc123DEF"
+    token = joat.issue_token()
+    self.assertIsNone(token)
+
+    joat.user_id = "12345"
+    token = joat.issue_token()
+    self.assertIsNone(token)
+
+    # this one actually should succeed, since it'll use datetime.now and the default lifetime
+    joat.scope = ['email', 'profile']
+    token = joat.issue_token()
+    self.assertIsNotNone(token)
